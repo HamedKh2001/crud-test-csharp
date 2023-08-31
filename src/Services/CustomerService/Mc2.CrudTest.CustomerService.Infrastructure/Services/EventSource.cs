a3b4b781d9396a8eb8ea1a421782327799d76fda
Hamed-Khorasani <Hamed.30sharp@gmail.com>
2023-08-31 13:47:52 +0330

    add event source

---
+++ src/Services/CustomerService/Mc2.CrudTest.CustomerService.Infrastructure/Services/EventSource.cs
@@ -0,0 +1,41 @@
+﻿using EventStore.ClientAPI;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace SharedKernel.Contracts.Infrastructure
+{
+    public class EventSource : IEventSource
+    {
+        private readonly IEventStoreConnection _connection;
+
+        public EventSource(IEventStoreConnection connection)
+        {
+            _connection = connection;
+        }
+        public void Save<TEvent>(string aggregateName, string streamId, IEnumerable<TEvent> events) where TEvent : IEvent
+        {
+            if (!events.Any()) return;
+
+            var changes = events
+                .Select(@event =>
+                    new EventData(
+                        eventId: Guid.NewGuid(),
+                        type: @event.GetType().Name,
+                        isJson: true,
+                        data: Serialize(@event),
+                        metadata: Serialize(new Events.EventMetadata
+                        { ClrType = @event.GetType().AssemblyQualifiedName })
+                    ))
+                .ToArray();
+
+            if (!changes.Any()) return;
+            var streamName = $"{aggregateName} - {streamId}";
+
+            _connection.AppendToStreamAsync(
+               streamName,
+               ExpectedVersion.Any,
+               changes).Wait();
+        }
+        private static byte[] Serialize(object data) => Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data));
+    }
+}
